be071f472582f9f294dd98ed28e5aeafa8781caa
anushka sagar <112419846+051821@users.noreply.github.com>
2023-01-11 19:00:06 +0530

    Create table.c
    Print a multiplication table of a number entered by user

---
+++ table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a,i=1;
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    do{
+        printf("%d\n",a*i);
+        i = i+1;
+
+    }while(i<11);
+    /* code */
+    return 0;
+}
